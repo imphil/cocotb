bcb62d78d14f14c32f807428c5debf885f0b6550
Philipp Wagner <mail@philipp-wagner.com>
2023-11-01 17:14:14 +0100

    DEBUG: Highlight callback threads in test_custom_entrypoint
    See also https://github.com/cocotb/cocotb/issues/3467

--- tests/test_cases/test_custom_entry/custom_entry.py
+++ tests/test_cases/test_custom_entry/custom_entry.py
@@ -1,13 +1,19 @@
+import sys
+import threading
 from typing import List
 
 from cocotb.log import _filter_from_c, _log_from_c  # noqa: F401
 
 
 def entry_func(argv: List[str]) -> None:
+    print("thread for entry_func: " + str(threading.current_thread()))
+    sys.stdout.flush()
     with open("results.log", "w") as file:
         print("got entry", file=file)
 
 
 def _sim_event(message: str) -> None:
+    print("thread for _sim_event: " + str(threading.current_thread()))
+    sys.stdout.flush()
     with open("results.log", "a") as file:
         print(f"got event message={message}", file=file)
